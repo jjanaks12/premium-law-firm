d44e2fc35c64b8328fcc9b87e536a39fdb672327
janak shrestha <jjanaks12@gmail.com>
2026-07-28 17:06:50 +0545

    feat: add Docker support for frontend and configure default admin permissions in seed script

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   turbopack: {
-    root: '/Users/janakshrestha/Documents/2026/07July/premium-law-firm',
+    root: path.join(__dirname, '..'),
   }
 };
 
